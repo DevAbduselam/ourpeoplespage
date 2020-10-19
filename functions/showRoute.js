778e6d8ec97e9a1705aeb86bcbd22219517b0b56
Abduselam <ethioabduselam@gmail.com>
2020-10-19 20:15:27 +0300

    try logging to telegram

--- functions/showRoute.js
+++ functions/showRoute.js
@@ -1,4 +1,5 @@
-const { writeFileSync, readFileSync, mkdirSync, existsSync } = require('fs');
+const { readFileSync } = require('fs');
+const { default: axios } = require("axios");
 const people = ['geddy', 'neil', 'alex'];
 const renderer = (templatePath, data) => {
   let cooo = readFileSync(templatePath).toString();
@@ -14,22 +15,41 @@ const renderer = (templatePath, data) => {
   }
   return cooo;
 }
+const BotToken = process.env.BOT_TOKEN;
 exports.handler = function (event, context, callback) {
   console.log('got request: ', JSON.stringify(event));
   const allowedPaths = ["mama", "baba", "dada", "nana"];
   const path = event.queryStringParameters.id.replace("/", "");
   console.log("path: ", path);
+  const url = `https://api.telegram.org/bot${BotToken}/sendMessage?text="path: ${path} request: ${JSON.stringify(event)}"&chat_id=471720649`;
   if (allowedPaths.includes(path)) {
-    return callback(null, {
-      statusCode: 200,
-      body: renderer('template.html', { person: path })
+    return axios.get(encodeURI(url)).then(r => {
+      return callback(null, {
+        statusCode: 200,
+        body: renderer('template.html', { person: path })
+      });
+    }).catch(err => {
+      return callback(null, {
+        statusCode: 200,
+        body: renderer("template.html", { person: path }),
+      });
     });
   }
-  return callback(null, {
-    body: JSON.stringify({error: 'sorry'}),
-    statusCode: 301,
-    headers: {
-      Location: `/404.html`,
-    }
-  });
+  return axios.get(encodeURI(url)).then(r => {
+    return callback(null, {
+      body: JSON.stringify({ error: "sorry" }),
+      statusCode: 301,
+      headers: {
+        Location: `/404.html`,
+      },
+    });
+    }).catch(err => {
+      return callback(null, {
+        body: JSON.stringify({ error: "sorry" }),
+        statusCode: 301,
+        headers: {
+          Location: `/404.html`,
+        },
+      });
+    });
 }
